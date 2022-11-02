62f04c4e4374dadb97659d8a86b18a6ad4b333b4
Hachem <106035779+ash-automates@users.noreply.github.com>
2022-11-02 23:14:22 +0100

    Implement ES-6 Non-included methods for Sets DS

--- 02_sets/mySetClass.js
+++ 02_sets/mySetClass.js
@@ -30,4 +30,41 @@ const MySet = function () {
   this.size = function () {
     return collection.length;
   };
+
+  this.union = function (otherSet) {
+    let unionSet = new MySet();
+    this.values().forEach((element) => {
+      unionSet.push(element);
+    });
+    otherSet.values().forEach((element) => {
+      unionSet.push(element);
+    });
+    return unionSet;
+  };
+
+  this.intersection = function (otherSet) {
+    let intersectionSet = new MySet();
+    this.values().forEach((element) => {
+      if (otherSet.has(element)) {
+        intersectionSet.push(element);
+      }
+    });
+    return intersectionSet;
+  };
+
+  this.difference = function (otherSet) {
+    let differenceSet = new MySet();
+    this.values().forEach((element) => {
+      if (!otherSet.has(element)) {
+        differenceSet.push(element);
+      }
+    });
+    return differenceSet;
+  };
+
+  this.subset = function (otherSet) {
+    return this.values().every((element) => {
+      return otherSet.has(element);
+    });
+  };
 };
