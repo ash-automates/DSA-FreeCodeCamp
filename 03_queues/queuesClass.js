e904b3cb34dc3f5ae3bc0cd3150322b9bd8f219c
Hachem <106035779+ash-automates@users.noreply.github.com>
2022-11-03 08:00:04 +0100

    Implement Queue data structure using javascript classes

---
+++ 03_queues/queuesClass.js
@@ -0,0 +1,23 @@
+const Queue = function () {
+  let collection = [];
+
+  this.print = function () {
+    console.log(collection);
+  };
+
+  this.enqueue = function (value) {
+    collection.push(value);
+  };
+
+  this.dequeue = function () {
+    return collection.shift();
+  };
+
+  this.front = function () {
+    return collection[0];
+  };
+
+  this.size = function () {
+    return collection.length;
+  };
+};
